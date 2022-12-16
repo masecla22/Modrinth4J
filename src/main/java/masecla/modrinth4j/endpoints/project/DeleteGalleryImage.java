de0496bf2800b2d6947ea9a3c1935398a6f2413e
masecla22 <supermasecla22@gmail.com>
2022-12-16 23:50:31 +0100

    Create DeleteGalleryImage.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/project/DeleteGalleryImage.java
@@ -0,0 +1,54 @@
+package masecla.modrinth4j.endpoints.project;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.project.DeleteGalleryImage.DeleteGalleryImageRequest;
+
+public class DeleteGalleryImage extends Endpoint<EmptyResponse, DeleteGalleryImageRequest> {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DeleteGalleryImageRequest {
+        private String url;
+    }
+
+    public DeleteGalleryImage(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/project/{id}/gallery";
+    }
+
+    @Override
+    public Class<DeleteGalleryImageRequest> getRequestClass() {
+        return DeleteGalleryImageRequest.class;
+    }
+
+    @Override
+    public Class<EmptyResponse> getResponseClass() {
+        return EmptyResponse.class;
+    }
+
+    @Override
+    public boolean isJsonBody() {
+        return false;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.DELETE;
+    }
+}
