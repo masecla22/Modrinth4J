10a2da92e956638b423c9009105d64c67484c980
masecla22 <supermasecla22@gmail.com>
2022-12-16 17:10:00 +0100

    Update SearchEndpoint.java

--- src/main/java/masecla/modrinth4j/endpoints/SearchEndpoint.java
+++ src/main/java/masecla/modrinth4j/endpoints/SearchEndpoint.java
@@ -1,13 +1,23 @@
 package masecla.modrinth4j.endpoints;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.SearchEndpoint.SearchRequest;
+import masecla.modrinth4j.endpoints.SearchEndpoint.SearchResponse;
+import masecla.modrinth4j.endpoints.SearchEndpoint.SearchResult;
+import masecla.modrinth4j.endpoints.generic.PaginatedEndpoint;
 import masecla.modrinth4j.endpoints.generic.PaginatedEndpoint.PaginatedRequest;
+import masecla.modrinth4j.endpoints.generic.PaginatedEndpoint.PaginatedResponse;
 import masecla.modrinth4j.model.project.ProjectType;
 import masecla.modrinth4j.model.project.SupportStatus;
+import masecla.modrinth4j.model.search.FacetCollection;
 
-public class SearchEndpoint {
+public class SearchEndpoint extends PaginatedEndpoint<SearchResult, SearchResponse, SearchRequest> {
     public static enum IndexType {
         @SerializedName("relevance")
         RELEVANCE,
@@ -63,8 +73,42 @@ public class SearchEndpoint {
         private String[] gallery;
     }
 
+    @Data
+    @SuperBuilder
+    @EqualsAndHashCode(callSuper = true)
     public static class SearchRequest extends PaginatedRequest {
         private String query;
-        
+        private IndexType index;
+        private FacetCollection facets;
     }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    public static class SearchResponse extends PaginatedResponse<SearchResult> {
+    }
+
+    public SearchEndpoint(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/search";
+    }
+
+    @Override
+    public Class<SearchRequest> getRequestClass() {
+        return SearchRequest.class;
+    }
+
+    @Override
+    public Class<SearchResponse> getResponseClass() {
+        return SearchResponse.class;
+    }
+
+    @Override
+    public boolean isJsonBody() {
+        return false;
+    }
+
 }
