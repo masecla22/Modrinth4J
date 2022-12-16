52d689341736ad1a7c9f006b936800a03471265f
masecla22 <supermasecla22@gmail.com>
2022-12-16 17:08:53 +0100

    Added support for URL queried parameters

--- src/main/java/masecla/modrinth4j/endpoints/generic/Endpoint.java
+++ src/main/java/masecla/modrinth4j/endpoints/generic/Endpoint.java
@@ -1,11 +1,14 @@
 package masecla.modrinth4j.endpoints.generic;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.jsoup.Connection.Response;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,11 +24,34 @@ public abstract class Endpoint<O, I> {
 
     public abstract String getEndpoint();
 
+    /**
+     * Whether or not the request body should be JSON.
+     * If this is false, the request will embed the parameters in the URL.
+     */
+    public boolean isJsonBody() {
+        return true;
+    }
+
     public CompletableFuture<O> sendRequest(I parameters) {
         return client.connect(getEndpoint()).thenApply(c -> {
             try {
-                String jsonBody = this.gson.toJson(parameters, getRequestClass());
-                Response response = c.requestBody(jsonBody).execute();
+                JsonElement jsonBody = gson.toJsonTree(parameters, getRequestClass());
+                // Response response = c.requestBody(jsonBody).execute();
+                if (isJsonBody()) {
+                    c.requestBody(jsonBody.toString());
+                } else {
+                    Map<String, String> data = new HashMap<>();
+                    for (Map.Entry<String, JsonElement> entry : jsonBody.getAsJsonObject().entrySet()) {
+                        if (entry.getValue().isJsonPrimitive()) {
+                            data.put(entry.getKey(), entry.getValue().getAsString());
+                        } else {
+                            data.put(entry.getKey(), entry.getValue().toString());
+                        }
+                    }
+                    c.data(data);
+                }
+
+                Response response = c.ignoreContentType(true).execute();
 
                 O object = this.gson.fromJson(response.body(), getResponseClass());
                 return object;
@@ -33,10 +59,14 @@ public abstract class Endpoint<O, I> {
                 e.printStackTrace();
                 return null;
             }
+        }).exceptionally(c -> {
+            c.printStackTrace();
+            return null;
         });
     }
 
     public abstract Class<O> getResponseClass();
 
     public abstract Class<I> getRequestClass();
+
 }
