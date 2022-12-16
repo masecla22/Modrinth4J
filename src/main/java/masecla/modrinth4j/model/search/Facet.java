9119c32c63cddbcb3cc2601a47e2af4743236231
masecla22 <supermasecla22@gmail.com>
2022-12-16 17:08:30 +0100

    Changed how facets are built

--- src/main/java/masecla/modrinth4j/model/search/Facet.java
+++ src/main/java/masecla/modrinth4j/model/search/Facet.java
@@ -1,10 +1,16 @@
 package masecla.modrinth4j.model.search;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import masecla.modrinth4j.model.project.ProjectType;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Facet {
-    
+
     public static enum FacetType {
         CATEGORIES,
         VERSIONS,
@@ -18,5 +24,21 @@ public class Facet {
     @Override
     public String toString() {
         return type.toString().toLowerCase() + ":" + value;
+    }
+
+    public static Facet category(String value) {
+        return new Facet(FacetType.CATEGORIES, value);
+    }
+
+    public static Facet version(String value) {
+        return new Facet(FacetType.VERSIONS, value);
+    }
+
+    public static Facet license(String value) {
+        return new Facet(FacetType.LICENSE, value);
+    }
+
+    public static Facet projectType(ProjectType type) {
+        return new Facet(FacetType.PROJECT_TYPE, type.name().toLowerCase());
     }
 }
