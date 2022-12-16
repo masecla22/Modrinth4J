e7609b59a99d53526caf0c8d5ce7a714aae38cfa
masecla22 <supermasecla22@gmail.com>
2022-12-16 17:08:59 +0100

    Update PaginatedEndpoint.java

--- src/main/java/masecla/modrinth4j/endpoints/generic/PaginatedEndpoint.java
+++ src/main/java/masecla/modrinth4j/endpoints/generic/PaginatedEndpoint.java
@@ -1,27 +1,26 @@
 package masecla.modrinth4j.endpoints.generic;
 
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
 
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.endpoints.generic.PaginatedEndpoint.PaginatedRequest;
 import masecla.modrinth4j.endpoints.generic.PaginatedEndpoint.PaginatedResponse;
 
-public abstract class PaginatedEndpoint<T, O extends PaginatedResponse<T>, I extends PaginatedRequest>
-        extends Endpoint<O, I> {
+public abstract class PaginatedEndpoint<T, O extends PaginatedResponse<T>, I extends PaginatedRequest> extends Endpoint<O, I> {
     @Data
+    @SuperBuilder
     public static class PaginatedRequest {
         private int offset;
         private int limit;
     }
-
+    
     @Data
     public static class PaginatedResponse<T> {
         private int offset;
         private int limit;
 
-        @SerializedName("total_hits")
         private int totalHits;
 
         private T[] hits;
