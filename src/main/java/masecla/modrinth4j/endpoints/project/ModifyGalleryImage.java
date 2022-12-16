cbb87fde1851f673a50af856ce377f219f1be965
masecla22 <supermasecla22@gmail.com>
2022-12-16 23:46:01 +0100

    Create ModifyGalleryImage.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/project/ModifyGalleryImage.java
@@ -0,0 +1,83 @@
+package masecla.modrinth4j.endpoints.project;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.project.ModifyGalleryImage.ModifyGalleryImageRequest;
+
+public class ModifyGalleryImage extends Endpoint<EmptyResponse, ModifyGalleryImageRequest> {
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class ModifyGalleryImageRequest {
+        private String url;
+        private String title;
+        private boolean featured;
+        private String description;
+    }
+
+    public ModifyGalleryImage(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/project/{id}/gallery";
+    }
+
+    @Override
+    protected String getReplacedUrl(ModifyGalleryImageRequest request, Map<String, String> parameters) {
+        // This is a bit of a hack, but it works.
+        // This is due to the fact that, for some reason the Modrinth API takes query
+        // parameters instead of a body
+        // for a PATCH request. (which is not what the HTTP spec says)
+
+        String endpoint = super.getReplacedUrl(request, parameters);
+        List<String> encoded = new ArrayList<>();
+        JsonObject object = getGson().toJsonTree(request).getAsJsonObject();
+        for (String key : object.keySet()) {
+            try {
+                encoded.add(key + "=" + URLEncoder.encode(object.get(key).getAsString(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+            }
+        }
+
+        return endpoint + "?" + String.join("&", encoded);
+
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.PATCH;
+    }
+
+    @Override
+    public Class<ModifyGalleryImageRequest> getRequestClass() {
+        return ModifyGalleryImageRequest.class;
+    }
+
+    @Override
+    public Class<EmptyResponse> getResponseClass() {
+        return EmptyResponse.class;
+    }
+
+    @Override
+    public boolean requiresBody() {
+        return false;
+    }
+
+}
