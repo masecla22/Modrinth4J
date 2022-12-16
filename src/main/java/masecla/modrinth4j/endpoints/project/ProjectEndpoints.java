8f58491c021f262fbfbe2cb9bf41ae58efcecc6d
masecla22 <supermasecla22@gmail.com>
2022-12-16 19:16:47 +0100

    Update ProjectEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/project/ProjectEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/ProjectEndpoints.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.project.GetMultipleProjects.GetMultipleProjectsRequest;
 import masecla.modrinth4j.endpoints.project.ModifyProject.ModifyProjectRequest;
 import masecla.modrinth4j.model.project.Project;
 
@@ -22,6 +23,11 @@ public class ProjectEndpoints {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", project);
         return new GetProject(client, gson).sendRequest(new EmptyRequest(), parameters);
+    }
+
+    public CompletableFuture<Project[]> get(String... project) {
+        return new GetMultipleProjects(client, gson)
+                .sendRequest(new GetMultipleProjectsRequest(project));
     }
 
     public CompletableFuture<ModifyProjectRequest> modify(String project, ModifyProjectRequest request) {
