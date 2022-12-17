e132faaf382c5ae276b61bf6001d93bd26ad01fc
masecla22 <supermasecla22@gmail.com>
2022-12-17 19:52:20 +0100

    Create RemoveMemberFromTeam.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/teams/RemoveMemberFromTeam.java
@@ -0,0 +1,37 @@
+package masecla.modrinth4j.endpoints.teams;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+
+public class RemoveMemberFromTeam extends Endpoint<EmptyResponse, EmptyRequest> {
+
+    public RemoveMemberFromTeam(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/team/{id}/members/{user}";
+    }
+
+    @Override
+    public Class<EmptyRequest> getRequestClass() {
+        return EmptyRequest.class;
+    }
+
+    @Override
+    public Class<EmptyResponse> getResponseClass() {
+        return EmptyResponse.class;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.DELETE;
+    }
+}
