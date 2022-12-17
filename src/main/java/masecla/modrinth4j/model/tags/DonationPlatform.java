ce1cff837e65ea75e8245f20bc0e118405924e5e
masecla22 <supermasecla22@gmail.com>
2022-12-17 20:32:57 +0100

    Create DonationPlatform.java

---
+++ src/main/java/masecla/modrinth4j/model/tags/DonationPlatform.java
@@ -0,0 +1,17 @@
+package masecla.modrinth4j.model.tags;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DonationPlatform {
+    @SerializedName("short")
+    private String shortName;
+    private String name;
+}
