1e9027a657e8dd45e3564d247fefa458b8a36653
masecla22 <supermasecla22@gmail.com>
2022-12-17 18:25:13 +0100

    Create ReportProjectUserOrVersion.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/user/ReportProjectUserOrVersion.java
@@ -0,0 +1,93 @@
+package masecla.modrinth4j.endpoints.user;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.user.ReportProjectUserOrVersion.ReportProjectUserOrVersionRequest;
+import masecla.modrinth4j.endpoints.user.ReportProjectUserOrVersion.ReportProjectUserOrVersionResponse;
+
+public class ReportProjectUserOrVersion
+        extends Endpoint<ReportProjectUserOrVersionResponse, ReportProjectUserOrVersionRequest> {
+    public static enum ReportedObjectType {
+        @SerializedName("project")
+        PROJECT,
+        @SerializedName("user")
+        USER,
+        @SerializedName("version")
+        VERSION;
+    }
+
+    public static enum ReportType {
+        @SerializedName("spam")
+        SPAM,
+        @SerializedName("copyright")
+        COPYRIGHT,
+        @SerializedName("inappropriate")
+        INAPPROPRIATE,
+        @SerializedName("malicious")
+        MALICIOUS,
+        @SerializedName("name-squatting")
+        NAME_SQUATTING,
+        @SerializedName("other")
+        OTHER;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReportProjectUserOrVersionRequest {
+        private ReportType reportType;
+        private String itemId;
+        private ReportedObjectType itemType;
+        private String body;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReportProjectUserOrVersionResponse {
+        private ReportType reportType;
+        private String itemId;
+        private ReportedObjectType itemType;
+        private String body;
+
+        private String reporter;
+
+        /** ISO 8601 String */
+        private String created;
+    }
+
+    public ReportProjectUserOrVersion(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/report";
+    }
+
+    @Override
+    public Class<ReportProjectUserOrVersionRequest> getRequestClass() {
+        return ReportProjectUserOrVersionRequest.class;
+    }
+
+    @Override
+    public Class<ReportProjectUserOrVersionResponse> getResponseClass() {
+        return ReportProjectUserOrVersionResponse.class;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.POST;
+    }
+}
