3cc05de375b39117452ad43602cd77b0df7457fa
masecla22 <supermasecla22@gmail.com>
2022-12-17 18:49:51 +0100

    Update TeamsEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/teams/TeamsEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/teams/TeamsEndpoints.java
@@ -1,12 +1,25 @@
 package masecla.modrinth4j.endpoints.teams;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 import com.google.gson.Gson;
 
 import lombok.AllArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
+import masecla.modrinth4j.model.team.ModrinthTeamMember;
 
 @AllArgsConstructor
 public class TeamsEndpoints {
     private Gson gson;
     private HttpClient client;
+
+    public CompletableFuture<ModrinthTeamMember[]> getTeamMembers(String projectId) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("id", projectId);
+
+        return new GetProjectTeamMembers(client, gson).sendRequest(new EmptyRequest(), parameters);
+    }
 }
