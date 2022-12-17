e070436b7a1a41b3d8633dd982bee2b4777c4fde
masecla22 <supermasecla22@gmail.com>
2022-12-17 01:58:04 +0100

    Update VersionFilesEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/VersionFilesEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/VersionFilesEndpoints.java
@@ -1,12 +1,26 @@
 package masecla.modrinth4j.endpoints.version.files;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 import com.google.gson.Gson;
 
 import lombok.AllArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.version.files.GetVersionByHash.GetVersionByHashRequest;
+import masecla.modrinth4j.model.version.FileHash;
+import masecla.modrinth4j.model.version.ProjectVersion;
 
 @AllArgsConstructor
 public class VersionFilesEndpoints {
     private Gson gson;
     private HttpClient client;
+
+    public CompletableFuture<ProjectVersion> getVersionByHash(String hash, FileHash algorithm){
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("hash", hash);
+
+        return new GetVersionByHash(client, gson).sendRequest(new GetVersionByHashRequest(algorithm), parameters);
+    }
 }
