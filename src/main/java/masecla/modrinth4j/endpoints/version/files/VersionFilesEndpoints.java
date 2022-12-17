277774178b8a0b644e81a8738a17cd10653758b1
masecla22 <supermasecla22@gmail.com>
2022-12-17 02:06:11 +0100

    Update VersionFilesEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/VersionFilesEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/VersionFilesEndpoints.java
@@ -8,6 +8,8 @@ import com.google.gson.Gson;
 
 import lombok.AllArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.version.files.DeleteFileByHash.DeleteFileByHashRequest;
 import masecla.modrinth4j.endpoints.version.files.GetVersionByHash.GetVersionByHashRequest;
 import masecla.modrinth4j.model.version.FileHash;
 import masecla.modrinth4j.model.version.ProjectVersion;
@@ -17,10 +19,17 @@ public class VersionFilesEndpoints {
     private Gson gson;
     private HttpClient client;
 
-    public CompletableFuture<ProjectVersion> getVersionByHash(String hash, FileHash algorithm){
+    public CompletableFuture<ProjectVersion> getVersionByHash(String hash, FileHash algorithm) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("hash", hash);
 
         return new GetVersionByHash(client, gson).sendRequest(new GetVersionByHashRequest(algorithm), parameters);
+    }
+
+    public CompletableFuture<EmptyResponse> deleteFileByHash(String hash, FileHash algorithm) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("hash", hash);
+
+        return new DeleteFileByHash(client, gson).sendRequest(new DeleteFileByHashRequest(algorithm), parameters);
     }
 }
