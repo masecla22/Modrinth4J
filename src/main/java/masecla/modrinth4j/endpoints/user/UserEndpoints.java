8aa9c21a3d4a18ed99d30b07ccf166043dc3eb82
masecla22 <supermasecla22@gmail.com>
2022-12-17 13:35:44 +0100

    Update UserEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/user/UserEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/user/UserEndpoints.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.user.GetUsers.GetUsersRequest;
 import masecla.modrinth4j.endpoints.user.ModifyUser.ModifyUserRequest;
 import masecla.modrinth4j.model.user.ModrinthUser;
 
@@ -18,11 +19,26 @@ public class UserEndpoints {
     private Gson gson;
     private HttpClient client;
 
+    /**
+     * Gets a user by their ID or username
+     * @param id - The ID or username of the user
+     * @return - The user
+     */
     public CompletableFuture<ModrinthUser> getUser(String id) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
 
         return new GetUser(client, gson).sendRequest(new EmptyRequest(), parameters);
+    }
+
+    /**
+     * Gets multiple users by their IDs. <b> Note: This will only work with IDs, not
+     * usernames. </b> unlike {@link #getUser(String)}
+     * @param ids - The IDs of the users
+     * @return - The users
+     */
+    public CompletableFuture<ModrinthUser[]> getUsers(String... ids) {
+        return new GetUsers(client, gson).sendRequest(new GetUsersRequest(ids));
     }
 
     public CompletableFuture<EmptyResponse> modifyUser(String id, ModifyUserRequest request) {
