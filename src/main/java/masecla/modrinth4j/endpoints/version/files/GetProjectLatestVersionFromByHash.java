0f21bba3e60084fbfd15f3eb9bd25d31a3ebdabd
masecla22 <supermasecla22@gmail.com>
2022-12-17 02:17:11 +0100

    Create GetProjectLatestVersionFromByHash.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionFromByHash.java
@@ -0,0 +1,75 @@
+package masecla.modrinth4j.endpoints.version.files;
+
+import java.util.Map;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.version.files.GetProjectLatestVersionFromByHash.GetProjectLatestVersionFromByHashRequest;
+import masecla.modrinth4j.model.version.FileHash;
+import masecla.modrinth4j.model.version.ProjectVersion;
+
+public class GetProjectLatestVersionFromByHash
+        extends Endpoint<ProjectVersion, GetProjectLatestVersionFromByHashRequest> {
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetProjectLatestVersionFromByHashRequest {
+        // Marked as transient to exclude it from the JSON body. It will be injected
+        // into the URL instead later.
+        private transient FileHash algorithm;
+
+        @Default
+        private String[] loaders = new String[0];
+        
+        @Default
+        private String[] gameVersions = new String[0];
+    }
+
+    public GetProjectLatestVersionFromByHash(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    protected String getReplacedUrl(GetProjectLatestVersionFromByHashRequest request, Map<String, String> parameters) {
+        // We can hook into this method to inject the query parameters.
+        // This is a bit of a hack, but it works.
+        // Why on earth this is not a part of the request class is beyond me.
+
+        String url = super.getReplacedUrl(request, parameters);
+        url += "?algorithm=" + request.algorithm.name().toLowerCase();
+        return url;
+    }
+
+    @Override
+    public String getEndpoint() {
+        // Why would you make a fetching endpoint a POST request with the end being
+        // /update. God damn it Modrinth.
+        return "/version_file/{hash}/update";
+    }
+
+    @Override
+    public Class<GetProjectLatestVersionFromByHashRequest> getRequestClass() {
+        return GetProjectLatestVersionFromByHashRequest.class;
+    }
+
+    @Override
+    public Class<ProjectVersion> getResponseClass() {
+        return ProjectVersion.class;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.POST;
+    }
+}
