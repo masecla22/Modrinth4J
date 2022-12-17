3d3f8a153f13a5b337761f648eb5c83cbb8fef17
masecla22 <supermasecla22@gmail.com>
2022-12-17 19:47:23 +0100

    Create ModifyTeamMemberInfo.java

---
+++ src/main/java/masecla/modrinth4j/endpoints/teams/ModifyTeamMemberInfo.java
@@ -0,0 +1,52 @@
+package masecla.modrinth4j.endpoints.teams;
+
+import org.jsoup.Connection.Method;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import masecla.modrinth4j.client.HttpClient;
+import masecla.modrinth4j.endpoints.generic.Endpoint;
+import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
+import masecla.modrinth4j.endpoints.teams.ModifyTeamMemberInfo.ModifyTeamMemberInfoRequest;
+import masecla.modrinth4j.model.team.ModrinthPermissionMask;
+
+public class ModifyTeamMemberInfo extends Endpoint<EmptyResponse, ModifyTeamMemberInfoRequest> {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ModifyTeamMemberInfoRequest {
+        private String role;
+        private ModrinthPermissionMask permissions;
+        private double payoutsSplit;
+    }
+
+    public ModifyTeamMemberInfo(HttpClient client, Gson gson) {
+        super(client, gson);
+    }
+
+    @Override
+    public String getEndpoint() {
+        return "/team/{id}/members/{user}";
+    }
+
+    @Override
+    public Class<ModifyTeamMemberInfoRequest> getRequestClass() {
+        return ModifyTeamMemberInfoRequest.class;
+    }
+
+    @Override
+    public Class<EmptyResponse> getResponseClass() {
+        return EmptyResponse.class;
+    }
+
+    @Override
+    public Method getMethod() {
+        return Method.PATCH;
+    }
+}
