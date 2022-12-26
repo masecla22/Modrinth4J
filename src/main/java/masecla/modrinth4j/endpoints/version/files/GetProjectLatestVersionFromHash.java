ab6b7b0fc6c180fc16838c700c4d343fe86a074e
masecla22 <supermasecla22@gmail.com>
2022-12-26 13:36:18 +0100

    Update GetProjectLatestVersionFromHash.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionFromHash.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionFromHash.java
@@ -1,7 +1,5 @@
 package masecla.modrinth4j.endpoints.version.files;
 
-import java.util.Map;
-
 import com.google.gson.Gson;
 
 import lombok.AllArgsConstructor;
@@ -12,7 +10,6 @@ import lombok.NoArgsConstructor;
 import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.version.files.GetProjectLatestVersionFromHash.GetProjectLatestVersionFromHashRequest;
-import masecla.modrinth4j.model.version.FileHash;
 import masecla.modrinth4j.model.version.ProjectVersion;
 
 public class GetProjectLatestVersionFromHash
@@ -23,10 +20,6 @@ public class GetProjectLatestVersionFromHash
     @NoArgsConstructor
     @AllArgsConstructor
     public static class GetProjectLatestVersionFromHashRequest {
-        // Marked as transient to exclude it from the JSON body. It will be injected
-        // into the URL instead later.
-        private transient FileHash algorithm;
-
         @Default
         private String[] loaders = new String[0];
 
@@ -39,21 +32,10 @@ public class GetProjectLatestVersionFromHash
     }
 
     @Override
-    protected String getReplacedUrl(GetProjectLatestVersionFromHashRequest request, Map<String, String> parameters) {
-        // We can hook into this method to inject the query parameters.
-        // This is a bit of a hack, but it works.
-        // Why on earth this is not a part of the request class is beyond me.
-
-        String url = super.getReplacedUrl(request, parameters);
-        url += "?algorithm=" + request.algorithm.name().toLowerCase();
-        return url;
-    }
-
-    @Override
     public String getEndpoint() {
         // Why would you make a fetching endpoint a POST request with the end being
         // /update. God damn it Modrinth.
-        return "/version_file/{hash}/update";
+        return "/version_file/{hash}/update?algorithm={algorithm}";
     }
 
     @Override
