6986820e8a995a2c6acabc2c5e3cd8e70428a0c9
masecla22 <supermasecla22@gmail.com>
2022-12-26 10:48:19 +0100

    Update ProjectDonationPlatform.java
    Added @Builder for consistency sake

--- src/main/java/masecla/modrinth4j/model/project/ProjectDonationPlatform.java
+++ src/main/java/masecla/modrinth4j/model/project/ProjectDonationPlatform.java
@@ -1,12 +1,14 @@
 package masecla.modrinth4j.model.project;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProjectDonationPlatform {
     private String id;
     private String platform;
