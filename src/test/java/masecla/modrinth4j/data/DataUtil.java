18e70f31652d1bec2741240db17596bbbe82db04
masecla22 <supermasecla22@gmail.com>
2022-12-26 12:16:07 +0100

    Update DataUtil.java

--- src/test/java/masecla/modrinth4j/data/DataUtil.java
+++ src/test/java/masecla/modrinth4j/data/DataUtil.java
@@ -9,10 +9,13 @@ import org.apache.commons.codec.digest.DigestUtils;
 import lombok.SneakyThrows;
 import masecla.modrinth4j.endpoints.project.CreateProject.CreateProjectRequest;
 import masecla.modrinth4j.endpoints.project.CreateProject.ProjectData;
+import masecla.modrinth4j.endpoints.version.CreateVersion.CreateVersionRequest;
 import masecla.modrinth4j.main.ModrinthAPI;
 import masecla.modrinth4j.model.project.Project;
 import masecla.modrinth4j.model.project.SupportStatus;
 import masecla.modrinth4j.model.tags.License;
+import masecla.modrinth4j.model.version.ProjectVersion;
+import masecla.modrinth4j.model.version.ProjectVersion.VersionType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -46,6 +49,19 @@ public class DataUtil {
         api.projects().delete("modrinth4j-test-project").join();
     }
 
+    public static ProjectVersion appendVersion(ModrinthAPI api, String projectId) {
+        return api.versions().createProjectVersion(CreateVersionRequest.builder()
+                .projectId(projectId)
+                .changelog("This is a changelog")
+                .gameVersions(new String[] { "1.16.5" })
+                .versionNumber("1.0.0")
+                .name("Version Name!")
+                .versionType(VersionType.RELEASE)
+                .loaders(new String[] { "spigot" })
+                .files(new File[] { getJar() })
+                .build()).join();
+    }
+
     public static File getImage() {
         return new File(DataUtil.class.getClassLoader().getResource("icon.png").getFile());
     }
@@ -58,12 +74,16 @@ public class DataUtil {
         return new File(DataUtil.class.getClassLoader().getResource("AntiCaps.jar").getFile());
     }
 
+    public static File getAnotherJar() {
+        return new File(DataUtil.class.getClassLoader().getResource("AntiCaps2.jar").getFile());
+    }
+
     /**
      * This method downloads the file at URL, and checks if the hash of the file is
      * identical to the hash of the file provided.
      */
     @SneakyThrows
-    public static boolean verifyIdentical(String url, File file){
+    public static boolean verifyIdentical(String url, File file) {
         OkHttpClient client = new OkHttpClient();
         Response response = client.newCall(new Request.Builder().url(url).build()).execute();
         InputStream stream = response.body().byteStream();
