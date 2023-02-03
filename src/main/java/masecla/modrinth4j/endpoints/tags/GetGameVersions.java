016e688ba4318418e1c88fb77d621af68b963157
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:25 +0100

    Update GetGameVersions.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetGameVersions.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetGameVersions.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.tags.GameVersion;
 
+/**
+ * This endpoint is used to get a list of game versions.
+ */
 public class GetGameVersions extends Endpoint<List<GameVersion>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetGameVersions(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/game_version";
     }
     
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<GameVersion>> getResponseClass() {
         return new TypeToken<List<GameVersion>>() {
