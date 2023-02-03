399d9e3b902e88b38f86b14736f58fcdfacb7fcb
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:24:26 +0100

    Update GetMultipleProjects.java

--- src/main/java/masecla/modrinth4j/endpoints/project/GetMultipleProjects.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/GetMultipleProjects.java
@@ -13,8 +13,14 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.project.GetMultipleProjects.GetMultipleProjectsRequest;
 import masecla.modrinth4j.model.project.Project;
 
+/**
+ * This endpoint is used to get multiple projects.
+ */
 public class GetMultipleProjects extends Endpoint<List<Project>, GetMultipleProjectsRequest> {
 
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -22,26 +28,44 @@ public class GetMultipleProjects extends Endpoint<List<Project>, GetMultipleProj
         private List<String> ids;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetMultipleProjects(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/projects";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetMultipleProjectsRequest> getRequestClass() {
         return TypeToken.get(GetMultipleProjectsRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<Project>> getResponseClass() {
         return new TypeToken<List<Project>>() {
         };
     }
 
+    /**
+     * Returns whether the endpoint requires a json body.
+     */
     @Override
     public boolean isJsonBody() {
         return false;
