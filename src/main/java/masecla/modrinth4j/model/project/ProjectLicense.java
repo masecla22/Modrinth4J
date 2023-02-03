fbced0b2003862a958b60073edfc81d5162ef414
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:58:08 +0100

    Update ProjectLicense.java

--- src/main/java/masecla/modrinth4j/model/project/ProjectLicense.java
+++ src/main/java/masecla/modrinth4j/model/project/ProjectLicense.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a project license.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectLicense {
+    /** The license's ID */
     private String id;
+    /** The license's name */
     private String name;
+    /** The license's URL */
     private String url;
 }
