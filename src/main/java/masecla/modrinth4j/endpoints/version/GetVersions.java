c9f5cdc41906859ca6bed7170889fd5aea9e1372
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:11:07 +0100

    Update GetVersions.java

--- src/main/java/masecla/modrinth4j/endpoints/version/GetVersions.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/GetVersions.java
@@ -13,8 +13,14 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.version.GetVersions.GetVersionsRequest;
 import masecla.modrinth4j.model.version.ProjectVersion;
 
+/**
+ * This endpoint is used to get a list of versions.
+ */
 public class GetVersions extends Endpoint<List<ProjectVersion>, GetVersionsRequest> {
 
+    /**
+     * This class is used to represent the request body.
+     */
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -22,26 +28,44 @@ public class GetVersions extends Endpoint<List<ProjectVersion>, GetVersionsReque
         private List<String> ids;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     *
+     * @param client The HTTP client to use.
+     * @param gson   The GSON instance to use.
+     */
     public GetVersions(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/versions";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetVersionsRequest> getRequestClass() {
         return TypeToken.get(GetVersionsRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<ProjectVersion>> getResponseClass() {
         return new TypeToken<List<ProjectVersion>>() {
         };
     }
 
+    /**
+     * Returns whether the request body is JSON.
+     */
     @Override
     public boolean isJsonBody() {
         return false;
