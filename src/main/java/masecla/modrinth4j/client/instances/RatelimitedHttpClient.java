eac940deed211d3783a5472d06feb65caa3fa913
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:25:22 +0100

    Update RatelimitedHttpClient.java

--- src/main/java/masecla/modrinth4j/client/instances/RatelimitedHttpClient.java
+++ src/main/java/masecla/modrinth4j/client/instances/RatelimitedHttpClient.java
@@ -7,19 +7,41 @@ import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.client.agent.UserAgent;
 import okhttp3.Response;
 
+/**
+ * This class is used to connect to the Modrinth API with rate limiting.
+ */
 public class RatelimitedHttpClient extends HttpClient {
+    /** The request limit. */
     private AtomicInteger requestLimit = new AtomicInteger(0);
+    /** The request count. */
     private AtomicInteger requestCount = new AtomicInteger(0);
+    /** The time left. */
     private AtomicInteger timeLeft = new AtomicInteger(0);
 
+    /**
+     * Creates a new instance of the client.
+     * 
+     * @param userAgent The user agent to use.
+     * @param apiKey    The API key to use.
+     */
     public RatelimitedHttpClient(UserAgent userAgent, String apiKey) {
         super(userAgent, apiKey);
     }
 
+    /**
+     * Creates a new instance of the client.
+     * 
+     * @param userAgent The user agent to use.
+     * @param baseUrl   The base URL to use.
+     * @param apiKey    The API key to use.
+     */
     public RatelimitedHttpClient(UserAgent userAgent, String baseUrl, String apiKey) {
         super(userAgent, baseUrl, apiKey);
     }
 
+    /**
+     * Returns the next request.
+     */
     @Override
     public CompletableFuture<Void> nextRequest() {
         // Check if we still have requests remaining
@@ -41,6 +63,9 @@ public class RatelimitedHttpClient extends HttpClient {
         }
     }
 
+    /**
+     * Intercepts the response.
+     */
     @Override
     public void interceptResponse(Response response) {
         requestLimit.set(Integer.parseInt(response.header("X-Ratelimit-Limit")));
