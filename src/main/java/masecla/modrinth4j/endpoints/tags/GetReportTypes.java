9843941da1849d48e704d191f7210b6ac3c41fa6
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:20 +0100

    Update GetReportTypes.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetReportTypes.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetReportTypes.java
@@ -9,22 +9,40 @@ import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 
+/**
+ * This endpoint is used to get a list of report types.
+ */
 public class GetReportTypes extends Endpoint<List<String>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetReportTypes(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/report_type";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<String>> getResponseClass() {
         return new TypeToken<List<String>>() {
