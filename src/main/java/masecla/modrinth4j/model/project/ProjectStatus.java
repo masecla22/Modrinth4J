9a0e63e637128966428cb2a0cb5d3f62728e9533
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:58:06 +0100

    Update ProjectStatus.java

--- src/main/java/masecla/modrinth4j/model/project/ProjectStatus.java
+++ src/main/java/masecla/modrinth4j/model/project/ProjectStatus.java
@@ -2,19 +2,29 @@ package masecla.modrinth4j.model.project;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Represents a project status.
+ */
 public enum ProjectStatus {
+    /** The project is approved and public. */
     @SerializedName("approved")
     APPROVED,
+    /** The project is rejected. */
     @SerializedName("rejected")
     REJECTED,
+    /** The project is a draft. */
     @SerializedName("draft")
     DRAFT,
+    /** The project is unlisted. */
     @SerializedName("unlisted")
     UNLISTED,
+    /** The project is archived. */
     @SerializedName("archived")
     ARCHIVED,
+    /** The project is processing. */
     @SerializedName("processing")
     PROCESSING,
+    /** The project is unknown. */
     @SerializedName("unknown")
     UNKNOWN;
 }
