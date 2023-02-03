c5dd9d093c5106f07f7e626c94b828fbd282557e
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:28 +0100

    Update GetCategories.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetCategories.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetCategories.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.tags.Category;
 
+/**
+ * This endpoint is used to get a list of categories.
+ */
 public class GetCategories extends Endpoint<List<Category>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetCategories(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/category";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<Category>> getResponseClass() {
         return new TypeToken<List<Category>>() {
