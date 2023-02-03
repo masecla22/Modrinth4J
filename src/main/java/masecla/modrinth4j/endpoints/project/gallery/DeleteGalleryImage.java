9cc28f1995e83901eba2a30907a91d8d4d180915
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:24:29 +0100

    Update DeleteGalleryImage.java

--- src/main/java/masecla/modrinth4j/endpoints/project/gallery/DeleteGalleryImage.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/gallery/DeleteGalleryImage.java
@@ -12,8 +12,14 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
 import masecla.modrinth4j.endpoints.project.gallery.DeleteGalleryImage.DeleteGalleryImageRequest;
 
+/**
+ * This endpoint is used to delete a gallery image.
+ */
 public class DeleteGalleryImage extends Endpoint<EmptyResponse, DeleteGalleryImageRequest> {
 
+    /**
+     * Represents the request.
+     */
     @Data
     @Builder
     @AllArgsConstructor
@@ -22,30 +28,51 @@ public class DeleteGalleryImage extends Endpoint<EmptyResponse, DeleteGalleryIma
         private String url;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public DeleteGalleryImage(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/project/{id}/gallery";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<DeleteGalleryImageRequest> getRequestClass() {
         return TypeToken.get(DeleteGalleryImageRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Whether or not the request is a json body.
+     */
     @Override
     public boolean isJsonBody() {
         return false;
     }
-
+    
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "DELETE";
