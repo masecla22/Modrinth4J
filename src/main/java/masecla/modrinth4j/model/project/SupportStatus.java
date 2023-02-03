d85b0dd323ffb238aedf491fec2c216692693b14
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:58:03 +0100

    Update SupportStatus.java

--- src/main/java/masecla/modrinth4j/model/project/SupportStatus.java
+++ src/main/java/masecla/modrinth4j/model/project/SupportStatus.java
@@ -2,11 +2,18 @@ package masecla.modrinth4j.model.project;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Represents a support status. This is used to determine if a mod supports a
+ * specific environment, such as CLIENT or SERVER.
+ */
 public enum SupportStatus {
+    /** The project is required to run under this. */
     @SerializedName("required")
     REQUIRED, 
+    /** The project is optional to run under this. */
     @SerializedName("optional")
     OPTIONAL,
+    /** The project does not support this. */
     @SerializedName("unsupported")
     UNSUPPORTED;
 }
