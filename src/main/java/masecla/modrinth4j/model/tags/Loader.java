cf7db5f4e141c5bd5b54f52d48097af098f6e5da
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:51:29 +0100

    Update Loader.java

--- src/main/java/masecla/modrinth4j/model/tags/Loader.java
+++ src/main/java/masecla/modrinth4j/model/tags/Loader.java
@@ -7,12 +7,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import masecla.modrinth4j.model.project.ProjectType;
 
+/**
+ * Represents a loader.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Loader {
+    /** The icon of the loader */
     private String icon;
+    /** The name of the loader */
     private String name;
+
+    /** The supported project types */
     private List<ProjectType> supportedProjectTypes;
 
 }
