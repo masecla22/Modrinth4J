cce0ac559a1d9b145b03740691e9bb76f049b55d
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:09 +0100

    Update JoinTeam.java

--- src/main/java/masecla/modrinth4j/endpoints/teams/JoinTeam.java
+++ src/main/java/masecla/modrinth4j/endpoints/teams/JoinTeam.java
@@ -8,27 +8,48 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
 
+/**
+ * This endpoint is used to join a team.
+ */
 public class JoinTeam extends Endpoint<EmptyResponse, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public JoinTeam(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/team/{id}/join";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "POST";
