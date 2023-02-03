3b461677e78d52be6a48845a3a1653bf541cd9a4
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:30:08 +0100

    Update GetUsers.java

--- src/main/java/masecla/modrinth4j/endpoints/user/GetUsers.java
+++ src/main/java/masecla/modrinth4j/endpoints/user/GetUsers.java
@@ -14,8 +14,14 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.user.GetUsers.GetUsersRequest;
 import masecla.modrinth4j.model.user.ModrinthUser;
 
+/**
+ * This endpoint is used to get a list of users.
+ */
 public class GetUsers extends Endpoint<List<ModrinthUser>, GetUsersRequest> {
 
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @NoArgsConstructor
@@ -24,26 +30,44 @@ public class GetUsers extends Endpoint<List<ModrinthUser>, GetUsersRequest> {
         public List<String> ids;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetUsers(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/users";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetUsersRequest> getRequestClass() {
         return TypeToken.get(GetUsersRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<ModrinthUser>> getResponseClass() {
         return new TypeToken<List<ModrinthUser>>() {
         };
     }
 
+    /**
+     * Returns whether or not the request is a json body.
+     */
     @Override
     public boolean isJsonBody() {
         return false;
