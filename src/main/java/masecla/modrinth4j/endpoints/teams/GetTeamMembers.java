dc37ba2fc74910a4be14f3a1fc43002ca2714cde
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:12 +0100

    Update GetTeamMembers.java

--- src/main/java/masecla/modrinth4j/endpoints/teams/GetTeamMembers.java
+++ src/main/java/masecla/modrinth4j/endpoints/teams/GetTeamMembers.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.team.ModrinthTeamMember;
 
+/**
+ * This endpoint is used to get a list of members from a team.
+ */
 public class GetTeamMembers extends Endpoint<List<ModrinthTeamMember>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetTeamMembers(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/team/{id}/members";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<ModrinthTeamMember>> getResponseClass() {
         return new TypeToken<List<ModrinthTeamMember>>() {
