fb8a795c7624a57fac9383fe47715e621ffd801d
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:20:26 +0100

    Update GetProjectLatestVersionFromHash.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionFromHash.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionFromHash.java
@@ -16,25 +16,42 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.version.files.GetProjectLatestVersionFromHash.GetProjectLatestVersionFromHashRequest;
 import masecla.modrinth4j.model.version.ProjectVersion;
 
+/**
+ * This endpoint is used to get the latest version from a hash.
+ */
 public class GetProjectLatestVersionFromHash
         extends Endpoint<ProjectVersion, GetProjectLatestVersionFromHashRequest> {
 
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class GetProjectLatestVersionFromHashRequest {
+        /** The loaders to check. */
         @Default
         private List<String> loaders = new ArrayList<>();
 
+        /** The game versions to check. */
         @Default
         private List<String> gameVersions = new ArrayList<>();
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetProjectLatestVersionFromHash(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         // Why would you make a fetching endpoint a POST request with the end being
@@ -42,16 +59,25 @@ public class GetProjectLatestVersionFromHash
         return "/version_file/{hash}/update?algorithm={algorithm}";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetProjectLatestVersionFromHashRequest> getRequestClass() {
         return TypeToken.get(GetProjectLatestVersionFromHashRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<ProjectVersion> getResponseClass() {
         return TypeToken.get(ProjectVersion.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "POST";
