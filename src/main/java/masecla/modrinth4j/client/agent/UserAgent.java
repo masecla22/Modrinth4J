96a24fdaef7fa0896ada348078231169fea07353
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:25:25 +0100

    Update UserAgent.java

--- src/main/java/masecla/modrinth4j/client/agent/UserAgent.java
+++ src/main/java/masecla/modrinth4j/client/agent/UserAgent.java
@@ -2,13 +2,23 @@ package masecla.modrinth4j.client.agent;
 
 import lombok.Builder;
 
+/**
+ * This class is used to represent a user agent.
+ */
 @Builder
 public class UserAgent {
+    /** The name of the project. */
     private String projectName;
+    /** The username of the author. */
     private String authorUsername;
+    /** The version of the project. */
     private String projectVersion;
+    /** The contact of the project. */
     private String contact;
 
+    /**
+     * Converts the user agent to a string.
+     */
     @Override
     public String toString() {
         if (projectName == null && authorUsername == null && contact == null) {
