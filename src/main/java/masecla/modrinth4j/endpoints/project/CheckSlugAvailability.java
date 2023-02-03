9ba9e677b9837a15049529ac51badf6c82b1831a
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:24:47 +0100

    Update CheckSlugAvailability.java

--- src/main/java/masecla/modrinth4j/endpoints/project/CheckSlugAvailability.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/CheckSlugAvailability.java
@@ -11,12 +11,24 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.project.CheckSlugAvailability.CheckSlugAvailabilityResponse;
 
+/**
+ * This endpoint is used to check if a slug is available.
+ */
 public class CheckSlugAvailability extends Endpoint<CheckSlugAvailabilityResponse, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public CheckSlugAvailability(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * This class is used to represent the response.
+     */
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -24,23 +36,34 @@ public class CheckSlugAvailability extends Endpoint<CheckSlugAvailabilityRespons
         private String id;
     }
 
+    /**
+     * Returns the endpoint.
+     */
     public String getEndpoint() {
         return "/project/{id}/check";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<CheckSlugAvailabilityResponse> getResponseClass() {
         return TypeToken.get(CheckSlugAvailabilityResponse.class);
     }
 
+    /**
+     * Returns if the endpoint requires a body.
+     */
     @Override
     public boolean requiresBody() {
         return false;
     }
-
 }
