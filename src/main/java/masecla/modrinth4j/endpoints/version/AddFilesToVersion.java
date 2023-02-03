69eb1fdcbe7b4478694e86d788603306ccfc5e38
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:16:03 +0100

    Update AddFilesToVersion.java

--- src/main/java/masecla/modrinth4j/endpoints/version/AddFilesToVersion.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/AddFilesToVersion.java
@@ -14,17 +14,32 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * This endpoint is used to add files to a version.
+ */
 public class AddFilesToVersion extends Endpoint<EmptyResponse, Map<String, InputStream>> {
 
+    /**
+     * Creates a new instance of this endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public AddFilesToVersion(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/version/{id}/file";
     }
 
+    /**
+     * Sends the request.
+     */
     @Override
     public CompletableFuture<EmptyResponse> sendRequest(Map<String, InputStream> request,
             Map<String, String> urlParams) {
@@ -49,17 +64,26 @@ public class AddFilesToVersion extends Endpoint<EmptyResponse, Map<String, Input
         });
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<Map<String, InputStream>> getRequestClass() {
         return new TypeToken<Map<String, InputStream>>() {
         };
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "POST";
