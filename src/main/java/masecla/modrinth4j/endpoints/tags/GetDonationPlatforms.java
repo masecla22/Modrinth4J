8cb58366ba95bbd00b0a4047c09eafa5eeeabe80
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:27 +0100

    Update GetDonationPlatforms.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetDonationPlatforms.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetDonationPlatforms.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.tags.DonationPlatform;
 
+/**
+ * This endpoint is used to get a list of donation platforms.
+ */
 public class GetDonationPlatforms extends Endpoint<List<DonationPlatform>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetDonationPlatforms(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/donation_platform";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<DonationPlatform>> getResponseClass() {
         return new TypeToken<List<DonationPlatform>>() {
