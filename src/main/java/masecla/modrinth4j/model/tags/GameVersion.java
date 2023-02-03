fb1c11dc887ca45037cc0f77f0e3cf836fc9bee9
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:51:34 +0100

    Update GameVersion.java

--- src/main/java/masecla/modrinth4j/model/tags/GameVersion.java
+++ src/main/java/masecla/modrinth4j/model/tags/GameVersion.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a game version.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GameVersion {
+    /** The game version's name */
     private String version;
+    /** The game version's type */
     private String versionType;
 
     /** ISO 8601 date */
     private String date;
+
+    /** If this is a major version */
     private boolean major;
 }
