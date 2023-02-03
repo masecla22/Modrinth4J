c45eca5e3e6c281fc7266f8c8b547a27de6cca07
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:25:20 +0100

    Update UnlimitedHttpClient.java

--- src/main/java/masecla/modrinth4j/client/instances/UnlimitedHttpClient.java
+++ src/main/java/masecla/modrinth4j/client/instances/UnlimitedHttpClient.java
@@ -5,16 +5,35 @@ import java.util.concurrent.CompletableFuture;
 import masecla.modrinth4j.client.HttpClient;
 import masecla.modrinth4j.client.agent.UserAgent;
 
+/**
+ * This class is used to connect to the Modrinth API without any rate limiting.
+ */
 public class UnlimitedHttpClient extends HttpClient {
 
+    /**
+     * Creates a new instance of the client.
+     * 
+     * @param userAgent The user agent to use.
+     * @param apiKey    The API key to use.
+     */
     public UnlimitedHttpClient(UserAgent userAgent, String apiKey) {
         super(userAgent, apiKey);
     }
 
+    /**
+     * Creates a new instance of the client.
+     * 
+     * @param userAgent The user agent to use.
+     * @param baseUrl   The base URL to use.
+     * @param apiKey    The API key to use.
+     */
     public UnlimitedHttpClient(UserAgent userAgent, String baseUrl, String apiKey) {
         super(userAgent, baseUrl, apiKey);
     }
 
+    /**
+     * Returns the next request.
+     */
     @Override
     public CompletableFuture<Void> nextRequest() {
         return CompletableFuture.completedFuture(null);
