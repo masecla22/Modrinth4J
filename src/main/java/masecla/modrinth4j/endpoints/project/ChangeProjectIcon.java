3de18b8770a74cf78d7f62aa3fee5e1a337e3a69
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:24:50 +0100

    Update ChangeProjectIcon.java

--- src/main/java/masecla/modrinth4j/endpoints/project/ChangeProjectIcon.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/ChangeProjectIcon.java
@@ -19,36 +19,64 @@ import masecla.modrinth4j.endpoints.project.ChangeProjectIcon.ChangeProjectIconR
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * This endpoint is used to change the icon of a project.
+ */
 public class ChangeProjectIcon extends Endpoint<EmptyResponse, ChangeProjectIconRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client - The client to use.
+     * @param gson - The gson instance to use.
+     */
     public ChangeProjectIcon(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Represents the data for a project.
+     */
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ChangeProjectIconRequest {
+        /** The input stream of the icon. */
         public InputStream icon;
+        /** The name of the icon. */
         public String iconName;
     }
 
+    /**
+     * Gets the endpoint of the request.
+     * 
+     * @return The endpoint of the request.
+     */
     @Override
     public String getEndpoint() {
         return "/project/{id}/icon";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<ChangeProjectIconRequest> getRequestClass() {
         return TypeToken.get(ChangeProjectIconRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Injects the file extension into the URL.
+     */
     @Override
     protected String getReplacedUrl(ChangeProjectIconRequest request, Map<String, String> parameters) {
         // Once again, Modrinth is a bit weird with their API. They use query parameters
@@ -58,6 +86,9 @@ public class ChangeProjectIcon extends Endpoint<EmptyResponse, ChangeProjectIcon
         return super.getReplacedUrl(request, parameters) + "?ext=" + ext;
     }
 
+    /**
+     * Sends the request.
+     */
     @Override
     public CompletableFuture<EmptyResponse> sendRequest(ChangeProjectIconRequest request, Map<String, String> urlParams) {
         String url = getReplacedUrl(request, urlParams);
@@ -77,6 +108,9 @@ public class ChangeProjectIcon extends Endpoint<EmptyResponse, ChangeProjectIcon
         });
     }
 
+    /**
+     * Returns the method of the request.
+     */
     @Override
     public String getMethod() {
         return "PATCH";
