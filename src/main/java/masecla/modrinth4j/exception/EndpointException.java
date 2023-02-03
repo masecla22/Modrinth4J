79f671ec802322a798408538987f256ee941e504
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:00:12 +0100

    Update EndpointException.java

--- src/main/java/masecla/modrinth4j/exception/EndpointException.java
+++ src/main/java/masecla/modrinth4j/exception/EndpointException.java
@@ -5,11 +5,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents an exception thrown by the Modrinth API.
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class EndpointException extends RuntimeException {
+    /** The error name. */
     private String error;
+
+    /** Further description in the error */
     private String description;
 }
