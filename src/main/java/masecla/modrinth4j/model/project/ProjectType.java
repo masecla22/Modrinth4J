8d74ef0b5ab5b59a621faaed4f68ce975f733cd9
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:58:05 +0100

    Update ProjectType.java

--- src/main/java/masecla/modrinth4j/model/project/ProjectType.java
+++ src/main/java/masecla/modrinth4j/model/project/ProjectType.java
@@ -2,11 +2,17 @@ package masecla.modrinth4j.model.project;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Represents a project type.
+ */
 public enum ProjectType {
+    /** A mod */
     @SerializedName("mod")
     MOD, 
+    /** A modpack */
     @SerializedName("resourcepack")
     MODPACK, 
+    /** A resourcepack */
     @SerializedName("modpack")
     RESOURCEPACK;
 }
