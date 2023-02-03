e3e8fa3ec4f3d76260159f39a682019b78859492
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:19:40 +0100

    Update GetProjectLatestVersionsFromHashes.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionsFromHashes.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/GetProjectLatestVersionsFromHashes.java
@@ -17,28 +17,48 @@ import masecla.modrinth4j.endpoints.version.files.GetProjectLatestVersionsFromHa
 import masecla.modrinth4j.model.version.FileHash;
 import masecla.modrinth4j.model.version.files.HashProjectVersionMap;
 
+/**
+ * This endpoint is used to get the latest versions from a list of hashes.
+ */
 public class GetProjectLatestVersionsFromHashes
         extends Endpoint<HashProjectVersionMap, GetProjectLatestVersionsFromHashesRequest> {
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     public static class GetProjectLatestVersionsFromHashesRequest {
+        /** The hash algorithm to use. */
         private FileHash algorithm;
+
+        /** The list of hashes to check. */
         @Default
         private List<String> hashes = new ArrayList<>();
 
+        /** The list of loaders to check. */
         @Default
         private List<String> loaders = new ArrayList<>();
 
+        /** The list of game versions to check. */
         @Default
         private List<String> gameVersions = new ArrayList<>();
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetProjectLatestVersionsFromHashes(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         // Once again, why you would make a POST request ending in /update for a
@@ -46,16 +66,25 @@ public class GetProjectLatestVersionsFromHashes
         return "/version_files/update";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetProjectLatestVersionsFromHashesRequest> getRequestClass() {
         return TypeToken.get(GetProjectLatestVersionsFromHashesRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<HashProjectVersionMap> getResponseClass() {
         return TypeToken.get(HashProjectVersionMap.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "POST";
