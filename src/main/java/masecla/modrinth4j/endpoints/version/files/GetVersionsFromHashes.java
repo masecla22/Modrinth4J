6206ec9680f1ae335698c382c326e83ffc112308
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:18:33 +0100

    Update GetVersionsFromHashes.java

--- src/main/java/masecla/modrinth4j/endpoints/version/files/GetVersionsFromHashes.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/files/GetVersionsFromHashes.java
@@ -15,7 +15,13 @@ import masecla.modrinth4j.endpoints.version.files.GetVersionsFromHashes.GetVersi
 import masecla.modrinth4j.model.version.FileHash;
 import masecla.modrinth4j.model.version.files.HashProjectVersionMap;
 
+/**
+ * This endpoint is used to get the versions from a list of hashes.
+ */
 public class GetVersionsFromHashes extends Endpoint<HashProjectVersionMap, GetVersionsFromHashesRequest> {
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @NoArgsConstructor
@@ -25,25 +31,43 @@ public class GetVersionsFromHashes extends Endpoint<HashProjectVersionMap, GetVe
         private List<String> hashes;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetVersionsFromHashes(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/version_files";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetVersionsFromHashesRequest> getRequestClass() {
         return TypeToken.get(GetVersionsFromHashesRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<HashProjectVersionMap> getResponseClass() {
         return TypeToken.get(HashProjectVersionMap.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "POST";
