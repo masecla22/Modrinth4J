cf7c821d4fc3af2b6b0dc9caa3f0dab3ba7d7ab6
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:49:47 +0100

    Update ModrinthUserNotification.java

--- src/main/java/masecla/modrinth4j/model/user/ModrinthUserNotification.java
+++ src/main/java/masecla/modrinth4j/model/user/ModrinthUserNotification.java
@@ -8,21 +8,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a notification for a user.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ModrinthUserNotification {
+    /** The id of the notification */
     private String id;
+
+    /** The id of the user this notification belongs to */
     private String userId;
 
     // TODO: It's fully undocumented, but finding all possible types should lead to
     // an enum here.
+    /** The notification type */
     private String type;
 
+    /** The title of the notification */
     private String title;
 
+    /** The text of the notification */
     private String text;
+
+    /** The link of the notification */
     private String link;
+
+    /** If the notification has been read */
     private boolean read;
 
     /** ISO 8601 String */
