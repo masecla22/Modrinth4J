6280464e123c8cfb90e90d5973334c0663fc4c6a
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:11:21 +0100

    Update GetVersion.java

--- src/main/java/masecla/modrinth4j/endpoints/version/GetVersion.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/GetVersion.java
@@ -8,22 +8,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.version.ProjectVersion;
 
+/**
+ * This endpoint is used to get a version.
+ */
 public class GetVersion extends Endpoint<ProjectVersion, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     *
+     * @param client The HTTP client to use.
+     * @param gson   The GSON instance to use.
+     */
     public GetVersion(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/version/{id}";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<ProjectVersion> getResponseClass() {
         return TypeToken.get(ProjectVersion.class);
