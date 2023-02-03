bb20e9cccf5a06aac0fc46e3835879e7767af94b
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:18 +0100

    Update GetLoaders.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetLoaders.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetLoaders.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.tags.Loader;
 
+/**
+ * This endpoint is used to get a list of loaders.
+ */
 public class GetLoaders extends Endpoint<List<Loader>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetLoaders(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/loader";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<Loader>> getResponseClass() {
         return new TypeToken<List<Loader>>() {
