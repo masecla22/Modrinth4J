68ef3354191f808387c85e3c4ee98e1d85baf898
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:22 +0100

    Update GetLicenses.java

--- src/main/java/masecla/modrinth4j/endpoints/tags/GetLicenses.java
+++ src/main/java/masecla/modrinth4j/endpoints/tags/GetLicenses.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.tags.License;
 
+/**
+ * This endpoint is used to get a list of licenses.
+ */
 public class GetLicenses extends Endpoint<List<License>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetLicenses(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/tag/license";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<License>> getResponseClass() {
         return new TypeToken<List<License>>() {
