17f42b1ff9da681e9c74d95e078f3d66d9c920a9
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:30:09 +0100

    Update GetUserProjects.java

--- src/main/java/masecla/modrinth4j/endpoints/user/GetUserProjects.java
+++ src/main/java/masecla/modrinth4j/endpoints/user/GetUserProjects.java
@@ -10,22 +10,40 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.model.project.Project;
 
+/**
+ * This endpoint is used to get a list of projects from a user.
+ */
 public class GetUserProjects extends Endpoint<List<Project>, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetUserProjects(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/user/{id}/projects";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<Project>> getResponseClass() {
         return new TypeToken<List<Project>>() {
