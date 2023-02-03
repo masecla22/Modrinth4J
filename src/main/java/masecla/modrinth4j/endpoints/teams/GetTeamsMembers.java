f239ec0548eb3a5f013a2e698cea847921c61f83
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:11 +0100

    Update GetTeamsMembers.java

--- src/main/java/masecla/modrinth4j/endpoints/teams/GetTeamsMembers.java
+++ src/main/java/masecla/modrinth4j/endpoints/teams/GetTeamsMembers.java
@@ -14,35 +14,60 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.teams.GetTeamsMembers.GetTeamsMembersRequest;
 import masecla.modrinth4j.model.team.ModrinthTeamMember;
 
+/**
+ * This endpoint is used to get a list of team members.
+ */
 public class GetTeamsMembers extends Endpoint<List<List<ModrinthTeamMember>>, GetTeamsMembersRequest> {
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class GetTeamsMembersRequest {
+        /** The list of team IDs to get the members of. */
         private List<String> ids;
     }
-    
+
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public GetTeamsMembers(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/teams";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<GetTeamsMembersRequest> getRequestClass() {
         return TypeToken.get(GetTeamsMembersRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<List<List<ModrinthTeamMember>>> getResponseClass() {
         return new TypeToken<List<List<ModrinthTeamMember>>>() {
         };
     }
 
+    /**
+     * Returns whether or not the request is a JSON body.
+     */
     @Override
     public boolean isJsonBody() {
         return false;
