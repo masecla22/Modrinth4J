1c6be1a7aa2513c837c243bdb7471810cb2cb16c
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:16:02 +0100

    Update CreateVersion.java

--- src/main/java/masecla/modrinth4j/endpoints/version/CreateVersion.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/CreateVersion.java
@@ -29,44 +29,71 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * This endpoint is used to create a new version.
+ */
 public class CreateVersion extends Endpoint<ProjectVersion, CreateVersionRequest> {
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CreateVersionRequest {
+        /** The name of the version */
         @NonNull
         private String name;
 
+        /** The version number of the version */
         @NonNull
         private String versionNumber;
 
+        /** The changelog of the version */
         @NonNull
         private String changelog;
 
+        /** The dependencies of the version */
         @Default
         private List<ProjectDependency> dependencies = new ArrayList<>();
+        /** The game versions of the version */
         private List<String> gameVersions;
+        /** The type of the version */
         private VersionType versionType;
 
+        /** The loaders of the version */
         @NonNull
         private List<String> loaders;
 
+        /** If the version is featured */
         private boolean featured;
 
+        /** The project ID of the version */
         @NonNull
         private String projectId;
 
+        /** The primary file of the version */
         private String primaryFile;
 
+        /** The files of the version */
         @NonNull
         private transient List<String> fileNames;
 
+        /** The file streams of the version */
         @NonNull
         private transient List<InputStream> fileStreams;
 
+        /**
+         * This class is used to build the request.
+         */
         public static class CreateVersionRequestBuilder {
 
+            /**
+             * This method is used to add files to the request.
+             * 
+             * @param files - The files to add.
+             * @return - The builder.
+             */
             public CreateVersionRequestBuilder files(File... files) {
                 try {
                     this.fileNames = new ArrayList<>();
@@ -82,21 +109,39 @@ public class CreateVersion extends Endpoint<ProjectVersion, CreateVersionRequest
                 return this;
             }
 
+            /**
+             * This method is used to add files to the request.
+             * 
+             * @param files - The files to add.
+             * @return - The builder.
+             */
             public CreateVersionRequestBuilder files(List<File> files) {
                 return this.files(files.toArray(new File[files.size()]));
             }
         }
     }
 
+    /**
+     * This constructor is used to create a new instance of the endpoint.
+     * 
+     * @param client - The client to use.
+     * @param gson   - The gson instance to use.
+     */
     public CreateVersion(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/version";
     }
 
+    /**
+     * This method will send the request.
+     */
     @Override
     public CompletableFuture<ProjectVersion> sendRequest(CreateVersionRequest request, Map<String, String> urlParams) {
         String url = getReplacedUrl(request, urlParams);
@@ -139,16 +184,25 @@ public class CreateVersion extends Endpoint<ProjectVersion, CreateVersionRequest
         });
     }
 
+    /**
+     * Returns the request class to use.
+     */
     @Override
     public TypeToken<CreateVersionRequest> getRequestClass() {
         return TypeToken.get(CreateVersionRequest.class);
     }
 
+    /**
+     * Returns the response class to use.
+     */
     @Override
     public TypeToken<ProjectVersion> getResponseClass() {
         return TypeToken.get(ProjectVersion.class);
     }
 
+    /**
+     * Returns the method to use.
+     */
     @Override
     public String getMethod() {
         return "POST";
