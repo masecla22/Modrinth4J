be0a6750d65a8952e4fced7e149d4f1459fd94bf
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:38:29 +0100

    Update TagsEndpointsTests.java

--- src/test/java/masecla/modrinth4j/client/TagsEndpointsTests.java
+++ src/test/java/masecla/modrinth4j/client/TagsEndpointsTests.java
@@ -8,9 +8,16 @@ import org.junit.Test;
 import masecla.modrinth4j.environment.EnvReader;
 import masecla.modrinth4j.main.ModrinthAPI;
 
+/**
+ * Tests the {@link TagsEndpoints} class.
+ */
 public class TagsEndpointsTests {
+    /** The client to be used */
     private ModrinthAPI client;
 
+    /**
+     * Sets up the client.
+     */
     @Before
     public void setupClient() {
         EnvReader env = new EnvReader();
@@ -18,31 +25,49 @@ public class TagsEndpointsTests {
 
     }
 
+    /**
+     * This method tests getting all the categories.
+     */
     @Test
     public void testGetCategories() {
         assertTrue(client.tags().getCategories().join() != null);
     }
 
+    /**
+     * This method tests getting all the donation platforms.
+     */
     @Test
     public void testGetDonationPlatforms() {
         assertTrue(client.tags().getDonationPlatforms().join() != null);
     }
 
+    /**
+     * This method tests getting all the loaders.
+     */
     @Test
     public void testGetLoaders() {
         assertTrue(client.tags().getLoaders().join() != null);
     }
 
+    /**
+     * This method tests getting all the game versions.
+     */
     @Test
     public void testGetGameVersions() {
         assertTrue(client.tags().getGameVersions().join() != null);
     }
 
+    /**
+     * This method tests getting all the licenses.
+     */
     @Test
     public void testGetLicenses() {
         assertTrue(client.tags().getLicenses().join() != null);
     }
 
+    /**
+     * This method tests getting all the mod types.
+     */
     @Test
     public void testGetReportTypes() {
         assertTrue(client.tags().getReportTypes().join() != null);
