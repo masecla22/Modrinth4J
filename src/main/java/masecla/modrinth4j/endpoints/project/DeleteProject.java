8b3ff2c9ee481822e70556c6205c9fbf4994c0b1
masecla22 <supermasecla22@gmail.com>
2023-02-03 13:24:37 +0100

    Update DeleteProject.java

--- src/main/java/masecla/modrinth4j/endpoints/project/DeleteProject.java
+++ src/main/java/masecla/modrinth4j/endpoints/project/DeleteProject.java
@@ -8,32 +8,56 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
 
+/**
+ * This endpoint is used to delete a project.
+ */
 public class DeleteProject extends Endpoint<EmptyResponse, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public DeleteProject(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/project/{id}";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Returns whether the endpoint requires a body.
+     */
     @Override
     public String getMethod() {
         return "DELETE";
     }
 
+    /**
+     * Returns whether the endpoint requires a body.
+     */
     @Override
     public boolean requiresBody() {
         return false;
