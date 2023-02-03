fe6c5a1e76d8eae15b27c18096b0dcf72bc78076
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:49:45 +0100

    Update ModrinthUserRole.java

--- src/main/java/masecla/modrinth4j/model/user/ModrinthUserRole.java
+++ src/main/java/masecla/modrinth4j/model/user/ModrinthUserRole.java
@@ -2,6 +2,9 @@ package masecla.modrinth4j.model.user;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Represents a user role on Modrinth.
+ */
 public enum ModrinthUserRole {
     @SerializedName("admin")
     ADMIN,
