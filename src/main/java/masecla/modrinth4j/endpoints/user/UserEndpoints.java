3ce471f103fcc172f9f7f3cc06cfb0908eb572d8
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:23:26 +0100

    Update UserEndpoints.java

--- src/main/java/masecla/modrinth4j/endpoints/user/UserEndpoints.java
+++ src/main/java/masecla/modrinth4j/endpoints/user/UserEndpoints.java
@@ -25,9 +25,14 @@ import masecla.modrinth4j.model.project.Project;
 import masecla.modrinth4j.model.user.ModrinthUser;
 import masecla.modrinth4j.model.user.ModrinthUserNotification;
 
+/**
+ * This class is used to represent the endpoints for users.
+ */
 @AllArgsConstructor
 public class UserEndpoints {
+    /** The Gson instance to use */
     private Gson gson;
+    /** The HTTP client to use */
     private HttpClient client;
 
     /**
@@ -65,6 +70,14 @@ public class UserEndpoints {
         return new GetUsers(client, gson).sendRequest(new GetUsersRequest(ids));
     };
 
+    /**
+     * Modifies a user by their ID
+     * 
+     * @param id      - The ID of the user
+     * @param request - The request to send
+     * @return - A {@link CompletableFuture} that will return an
+     *         {@link EmptyResponse} when complete
+     */
     public CompletableFuture<EmptyResponse> modifyUser(String id, ModifyUserRequest request) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -72,6 +85,13 @@ public class UserEndpoints {
         return new ModifyUser(client, gson).sendRequest(request, parameters);
     }
 
+    /**
+     * Deletes a user by their ID
+     * 
+     * @param id - The ID of the user
+     * @return - A {@link CompletableFuture} that will return an
+     *         {@link EmptyResponse} when complete
+     */
     public CompletableFuture<EmptyResponse> deleteUser(String id) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -79,14 +99,37 @@ public class UserEndpoints {
         return new DeleteUser(client, gson).sendRequest(new EmptyRequest(), parameters);
     }
 
+    /**
+     * Gets the current user from the API
+     * 
+     * @return - The current user
+     */
     public CompletableFuture<ModrinthUser> getSelf() {
         return new GetSelf(client, gson).sendRequest(new EmptyRequest(), new HashMap<>());
     }
 
+    /**
+     * Modifies the profile picture of a user by their ID
+     * 
+     * @param id   - The ID of the user
+     * @param file - The file to use as the profile picture
+     * @return - A {@link CompletableFuture} that will return an
+     *         {@link EmptyResponse} when complete
+     * @throws FileNotFoundException - If the file cannot be found
+     */
     public CompletableFuture<EmptyResponse> changeProfilePicture(String id, File file) throws FileNotFoundException {
         return changeProfilePicture(id, new FileInputStream(file), file.getName());
     }
 
+    /**
+     * Modifies the profile picture of a user by their ID
+     * 
+     * @param id       - The ID of the user
+     * @param stream   - The stream to use as the profile picture
+     * @param filename - The filename of the profile picture
+     * @return - A {@link CompletableFuture} that will return an
+     *         {@link EmptyResponse} when complete
+     */
     public CompletableFuture<EmptyResponse> changeProfilePicture(String id, InputStream stream, String filename) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -94,6 +137,13 @@ public class UserEndpoints {
         return new ChangeUserIcon(client, gson).sendRequest(new ChangeUserIconRequest(filename, stream), parameters);
     }
 
+    /**
+     * Gets the projects of a user by their ID
+     * 
+     * @param id - The ID of the user
+     * @return - A {@link CompletableFuture} that will return a {@link List} of
+     *         {@link Project}s when complete
+     */
     public CompletableFuture<List<Project>> getUserProjects(String id) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -101,6 +151,13 @@ public class UserEndpoints {
         return new GetUserProjects(client, gson).sendRequest(new EmptyRequest(), parameters);
     }
 
+    /**
+     * Gets the followed projects of a user by their ID
+     * 
+     * @param id - The ID of the user
+     * @return - A {@link CompletableFuture} that will return a {@link List} of
+     *         {@link Project}s when complete
+     */
     public CompletableFuture<List<Project>> getUserFollowedProjects(String id) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -108,6 +165,13 @@ public class UserEndpoints {
         return new GetUserFollowedProjects(client, gson).sendRequest(new EmptyRequest(), parameters);
     }
 
+    /**
+     * Gets the notifications of a user by their ID
+     * 
+     * @param id - The ID of the user
+     * @return - A {@link CompletableFuture} that will return a {@link List} of
+     *         {@link ModrinthUserNotification}s when complete
+     */
     public CompletableFuture<List<ModrinthUserNotification>> getNotifications(String id) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("id", id);
@@ -115,6 +179,13 @@ public class UserEndpoints {
         return new GetUserNotifications(client, gson).sendRequest(new EmptyRequest(), parameters);
     }
 
+    /**
+     * Reports a project, user, or version
+     * 
+     * @param request - The request to send
+     * @return - A {@link CompletableFuture} that will return a
+     *         {@link ReportProjectUserOrVersionResponse} when complete
+     */
     public CompletableFuture<ReportProjectUserOrVersionResponse> reportProjectUserOrVersion(
             ReportProjectUserOrVersionRequest request) {
         return new ReportProjectUserOrVersion(client, gson).sendRequest(request);
