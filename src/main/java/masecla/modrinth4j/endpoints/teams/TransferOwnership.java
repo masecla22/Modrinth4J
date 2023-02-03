78160023d2f8b4d0a21ec24f08ee1613e570b1ae
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:48:03 +0100

    Update TransferOwnership.java

--- src/main/java/masecla/modrinth4j/endpoints/teams/TransferOwnership.java
+++ src/main/java/masecla/modrinth4j/endpoints/teams/TransferOwnership.java
@@ -12,7 +12,13 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
 import masecla.modrinth4j.endpoints.teams.TransferOwnership.TransferOwnershipRequest;
 
+/**
+ * This endpoint is used to transfer ownership of a team.
+ */
 public class TransferOwnership extends Endpoint<EmptyResponse, TransferOwnershipRequest> {
+    /**
+     * This class is used to represent the request.
+     */
     @Data
     @Builder
     @AllArgsConstructor
@@ -21,25 +27,43 @@ public class TransferOwnership extends Endpoint<EmptyResponse, TransferOwnership
         public String userId;
     }
 
+    /**
+     * Creates a new instance of the endpoint.
+     * 
+     * @param client The client to use.
+     * @param gson   The gson instance to use.
+     */
     public TransferOwnership(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/team/{id}/owner";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<TransferOwnershipRequest> getRequestClass() {
         return TypeToken.get(TransferOwnershipRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "PATCH";
