5d5e27865e4ef33e2429a8c3965892bc71f94de2
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:26:38 +0100

    Starting to add full Javadoc everywhere so the build can pass

--- src/test/java/masecla/modrinth4j/environment/EnvReader.java
+++ src/test/java/masecla/modrinth4j/environment/EnvReader.java
@@ -4,15 +4,26 @@ import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
 import masecla.modrinth4j.client.agent.UserAgent;
 
+/**
+ * This class is used to read the environment variables from the .env file during testing.
+ */
 @Getter
 public class EnvReader {
+    /** The API key */
     private String apiKey;
+
+    /** The URL of the Modrinth server running the tests on */
     private String stagingUrl;
 
+    /** The UserAgent to identify with */
     private UserAgent agent;
 
+    /** 
+     * Creates a new EnvReader instance.
+     */
     public EnvReader() {
         Dotenv env = Dotenv.load();
+
         this.apiKey = env.get("MODRINTH_STAGING_TOKEN");
         this.stagingUrl = env.get("MODRINTH_STAGING_URL");
 
