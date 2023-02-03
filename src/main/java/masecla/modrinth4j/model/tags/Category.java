a0470dac161dc1c3726d6037a317a18e7c3de5ff
masecla22 <supermasecla22@gmail.com>
2023-02-03 11:51:37 +0100

    Update Category.java

--- src/main/java/masecla/modrinth4j/model/tags/Category.java
+++ src/main/java/masecla/modrinth4j/model/tags/Category.java
@@ -5,13 +5,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import masecla.modrinth4j.model.project.ProjectType;
 
+/**
+ * Represents a category.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Category {
+    /** The category's icon */
     private String icon;
+    /** The category's name */
     private String name;
+    /** The category's project type */
     private ProjectType projectType;
 
+    /** The category's header */
     private String header;
 }
