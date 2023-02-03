3bfc7713eff08da207cb05870bc9cc5b87147ad1
masecla22 <supermasecla22@gmail.com>
2023-02-03 12:12:42 +0100

    Update DeleteVersion.java

--- src/main/java/masecla/modrinth4j/endpoints/version/DeleteVersion.java
+++ src/main/java/masecla/modrinth4j/endpoints/version/DeleteVersion.java
@@ -8,27 +8,48 @@ import masecla.modrinth4j.endpoints.generic.Endpoint;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyRequest;
 import masecla.modrinth4j.endpoints.generic.empty.EmptyResponse;
 
+/**
+ * This endpoint is used to delete a version.
+ */
 public class DeleteVersion extends Endpoint<EmptyResponse, EmptyRequest> {
 
+    /**
+     * Creates a new instance of the endpoint.
+     *
+     * @param client The HTTP client to use.
+     * @param gson   The GSON instance to use.
+     */
     public DeleteVersion(HttpClient client, Gson gson) {
         super(client, gson);
     }
 
+    /**
+     * Returns the endpoint.
+     */
     @Override
     public String getEndpoint() {
         return "/version/{id}";
     }
 
+    /**
+     * Returns the request class.
+     */
     @Override
     public TypeToken<EmptyRequest> getRequestClass() {
         return TypeToken.get(EmptyRequest.class);
     }
 
+    /**
+     * Returns the response class.
+     */
     @Override
     public TypeToken<EmptyResponse> getResponseClass() {
         return TypeToken.get(EmptyResponse.class);
     }
 
+    /**
+     * Returns the method.
+     */
     @Override
     public String getMethod() {
         return "DELETE";
