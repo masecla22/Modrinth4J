c3e1c28285475e978db80d03fcee6d25d5bc360a
masecla22 <supermasecla22@gmail.com>
2024-01-30 13:16:52 +0100

    Update ProjectStatus.java

--- src/main/java/masecla/modrinth4j/model/project/ProjectStatus.java
+++ src/main/java/masecla/modrinth4j/model/project/ProjectStatus.java
@@ -2,29 +2,51 @@ package masecla.modrinth4j.model.project;
 
 import com.google.gson.annotations.SerializedName;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * Represents a project status.
  */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum ProjectStatus {
     /** The project is approved and public. */
     @SerializedName("approved")
-    APPROVED,
+    APPROVED(true),
     /** The project is rejected. */
     @SerializedName("rejected")
     REJECTED,
     /** The project is a draft. */
     @SerializedName("draft")
-    DRAFT,
+    DRAFT(true),
     /** The project is unlisted. */
     @SerializedName("unlisted")
-    UNLISTED,
+    UNLISTED(true),
     /** The project is archived. */
     @SerializedName("archived")
-    ARCHIVED,
+    ARCHIVED(true),
     /** The project is processing. */
     @SerializedName("processing")
     PROCESSING,
+
+    /** The project is withheld */
+    @SerializedName("withheld")
+    WITHHELD,
+
+    /** The project is scheduled */
+    @SerializedName("scheduled")
+    SCHEDULED,
+
+    /** The project is private */
+    @SerializedName("private")
+    PRIVATE(true),
+
     /** The project is unknown. */
     @SerializedName("unknown")
     UNKNOWN;
+
+    private boolean canBeRequested = false;
 }
